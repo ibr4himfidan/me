7a1896162709b8970905ff9f3a9fd5e6d87f0b3e
İbrahim Fidan <ibrahim.fidan@mysilo.com>
2019-03-06 07:59:59 +0300

    added code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './public/images/logo.jpeg';
+import './public/css/main.min.css';
+import './public/css/fontawesome-all.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="profile--image">
+          <div className="center">
+            <a href="https://www.dontsafe.com/"><img src={logo} alt="Profile Photo"/></a>
+          </div>
+        </div>
+        <div className="profile--text">
+          <div className="center">
+            <p>Full Stack Developer at <a href="https://www.mysilo.com/">Mysilo</a>, Ads Manager | Bug Researcher | Founder <a className="logo--color" href="#">Gecersiztik.com</a> </p>
+          </div>
+        </div>
+        <div className="profile--information">
+            <div className="center">
+            <ul>
+              <li><a href="#"><span className="fab fa-twitter"></span></a></li>
+              <li><a href="#"><span className="fab fa-instagram"></span></a></li>
+              <li><a href="#"><span className="fab fa-linkedin"></span></a></li>
+            </ul>
+            </div>
+        </div>
       </div>
     );
   }
