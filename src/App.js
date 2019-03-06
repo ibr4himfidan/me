e0a5424ca1bb315006b9ede256cda3d2c8051e81
İbrahim Fidan <ibrahim.fidan@mysilo.com>
2019-03-06 17:08:59 +0300

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './public/images/logo.jpeg';
 import './public/css/main.min.css';
 import './public/css/fontawesome-all.min.css';
 
@@ -9,20 +8,20 @@ class App extends Component {
       <div className="App">
         <div className="profile--image">
           <div className="center">
-            <a href="https://www.dontsafe.com/"><img src={logo} alt="Profile Photo"/></a>
+            <a href="https://www.dontsafe.com/"><img src="/public/images/logo.jpeg" alt=""/></a>
           </div>
         </div>
         <div className="profile--text">
           <div className="center">
-            <p>Full Stack Developer at <a href="https://www.mysilo.com/">Mysilo</a>, Ads Manager | Bug Researcher | Founder <a className="logo--color" href="#">Gecersiztik.com</a> </p>
+            <p>Full Stack Developer at <a href="https://www.mysilo.com/">Mysilo</a>, Ads Manager | Bug Researcher | Founder <a className="logo--color" href="https://www.gecerrsiztik.com/">Gecersiztik.com</a> </p>
           </div>
         </div>
         <div className="profile--information">
             <div className="center">
             <ul>
-              <li><a href="#"><span className="fab fa-twitter"></span></a></li>
-              <li><a href="#"><span className="fab fa-instagram"></span></a></li>
-              <li><a href="#"><span className="fab fa-linkedin"></span></a></li>
+              <li><a href="https://www.twitter.com/L4NETLY/"><span className="fab fa-twitter"></span></a></li>
+              <li><a href="https://www.instagram.com/ibr4himfidan/"><span className="fab fa-instagram"></span></a></li>
+              <li><a href="https://www.linkedin.com/in/ibrahim-fidan-4b173aab/"><span className="fab fa-linkedin"></span></a></li>
             </ul>
             </div>
         </div>
